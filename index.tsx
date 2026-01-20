c638ec94c9564f280d70a1b37b4f7568555fb3f9
cicybot <cicybot@outlook.com>
2026-01-20 20:42:15 +0630

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,14 +1,41 @@
 
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import ReactDOM from 'react-dom/client';
 import { RpcProvider } from './app/RpcContext';
-import { Render } from './app/Render.tsx';
 import { App } from "./app/App.tsx";
+import { Render } from "./app/Render.tsx";
+import  View from "./app/View.tsx";
 
+function InitWindow(){
+    const [bounds, setBounds] = useState({width :0,height:0,x:0,y:0})
+    useEffect(() => {
+        //@ts-ignore
+        window.__setBounds = (l)=>{
+            setBounds((r)=>{
+                return {
+                    ...r,...l,
+                }
+            })
+        }
+    }, []);
+    return  <View w100vw h100vh column jSpaceBetween>
+        <View rowVCenter jSpaceBetween>
+            <View p12>x:{bounds.x},y:{bounds.y}</View>
+            <View p12>x:{bounds.x +bounds.width},y:{bounds.y}</View>
+        </View>
+        <View rowVCenter center>width:{bounds.width},height:{bounds.height}</View>
+        <View rowVCenter jSpaceBetween>
+            <View p12>x:{bounds.x},y:{bounds.y + bounds.height}</View>
+            <View p12>x:{bounds.x +bounds.width},y:{bounds.y+ bounds.height}</View>
+        </View>
+    </View>
+}
 const Inner = () => {
     // If the URL contains "render", we show the specific window/desktop view
     if (location.href.indexOf("/render") > -1) {
         return <Render />;
+    }else if (location.href.indexOf("/initWindow") > -1) {
+        return <InitWindow />;
     } else {
         return <App />;
     }
